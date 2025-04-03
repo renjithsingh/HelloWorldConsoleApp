349de30805f5d3e0fc8217707ee6d0ba0353bbbf
devrenjith singh <devrenith_singh@epam.com>
2025-04-03 09:55:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using HelloWorldLibrary; // Reference to the .NET Standard library
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a username as a command line argument.");
+                return;
+            }
+
+            string username = args[0];
+            string message = HelloWorldHelper.GetGreetingMessage(username);
+            Console.WriteLine(message);
+        }
+    }
+}
